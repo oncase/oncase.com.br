5c8fad80d25444bf241db5aa474d2c005c1f1472
Marcello Pontes <balaum@gmail.com>
2015-06-26 22:56:00 +0200

    A little of jumbotron carousel shadowed-icon component

--- app/pages/home/homeCtrl.js
+++ app/pages/home/homeCtrl.js
@@ -7,14 +7,21 @@
   	$scope.myInterval = 5000;
 
   	$scope.slides = [
-  		{
-  			smallText : "Our Business is",
-  			bigText : "Intelligence"
-  		},
-  		{
-  			smallText : "Nosso neg\u00F3cio \u00E9", 
-  			bigText : "Intelig\u00EAncia"
-  		}
+      {
+        smallText : "Nosso neg\u00F3cio \u00E9", 
+        bigText : "Intelig\u00EAncia",
+        icon : "equalizer"
+      },
+      {
+        smallText : "Você está pronto para", 
+        bigText : "a Nuvem?",
+        icon : "cloud"
+      },
+      {
+        smallText : "Somos o único Pentaho Premium Partner", 
+        bigText : "do Brasil",
+        icon : "new_releases"
+      }
   	];
     
   });
