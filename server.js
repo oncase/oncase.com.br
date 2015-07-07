38f5ddb9a773b1e9481bb35ea3119c1a208d484a
Vitor Torreao <vitordeatorreao@gmail.com>
2015-07-07 12:43:24 -0300

    Added the other page's URLs to the rewrite rules

--- server.js
+++ server.js
@@ -6,8 +6,22 @@ var express = require('express');
 var rewrite = require('express-urlrewrite');
 var app = express();
 
+//Rewrite rules for URLs
 app.use(rewrite(/^\/about$/, '/#!/about'));
 app.use(rewrite(/^\/about\/$/, '/#!/about'));
+
+app.use(rewrite(/^\/services$/, '/#!/services'));
+app.use(rewrite(/^\/services\/$/, '/#!/services'));
+
+app.use(rewrite(/^\/products$/, '/#!/products'));
+app.use(rewrite(/^\/products\/$/, '/#!/products'));
+
+app.use(rewrite(/^\/clients-and-partners$/, '/#!/clients-and-partners'));
+app.use(rewrite(/^\/clients-and-partners\/$/, '/#!/clients-and-partners'));
+
+app.use(rewrite(/^\/contact$/, '/#!/contact'));
+app.use(rewrite(/^\/contact\/$/, '/#!/contact'));
+//End of rewrite rules
 
 app.use(express.static('./'));
 
