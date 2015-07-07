2f1a063b3fcbc3ee8f7b6d5015f6526f2b52be08
Vitor Torreao <vitor.albuquerque@oncase.com.br>
2015-07-07 08:57:01 -0300

    Added a rewrite library to package.json

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
-var connect = require('connect');
+/* var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic('./')).listen(8080);
+connect().use(serveStatic('./')).listen(8080); */
+
+var express = require('express');
+var rewrite = require('express-urlrewrite');
+var app = express();
+
+app.use(rewrite(/^\/about$/, '/#!/about'));
+app.use(rewrite(/^\/about\/$/, '/#!/about'));
+
+app.use(express.static('./'));
+
+app.listen(8080);
+console.log('Server running at http://localhost:8080/');
+
+module.exports = app;
