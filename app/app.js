653c5c4d4781d9a3c826fcb640aa7ac692fe4b5b
Marcello Pontes <balaum@gmail.com>
2015-07-15 14:15:37 +0200

    app.js - code beautified / Added oncase color pallete; home/index - added more content as well as some styles for contents / Changed cards style - it’s now flat and then pops out with shadow when mouse-over; bootstrap-grid-system.min.css - added Roboto as primary font-family option; oncase-global.css - added some global css information; still to be organized svgs - changed some and added new images index.html - upgraded angular material and added new heavy-weight for Roboto font

--- app/app.js
+++ app/app.js
@@ -1,61 +1,80 @@
-(function(){
+(function () {
 
   var app = angular.module(
-  	'oncase',
-  	[
-  		'ngMaterial',
-      'headerModule',
-      'ngRoute',
-      'ngAnimate',
-      'oncasePages',
-      'jumboBackgroundService','mouseOverClass'
-  	]
-  ).config(function($mdThemingProvider, $routeProvider, $locationProvider) {
+      'oncase', [
+        'ngMaterial',
+        'headerModule',
+        'ngRoute',
+        'ngAnimate',
+        'oncasePages',
+        'jumboBackgroundService', 'mouseOverClass'
+      ]
+    )
+    .config(function ($mdThemingProvider, $routeProvider, $locationProvider) {
+
+      $mdThemingProvider.definePalette('oncasePalette', {
+        '50': 'e5e9ef',
+        '100': 'bfc8d8',
+        '200': '95a4bf',
+        '300': '6f84a8',
+        '400': '4e6693',
+        '500': '2b4a7f',
+        '600': '284576',
+        '700': '203c6c',
+        '800': '1b3462',
+        '900': '0e2854',
+        'A100': '82a5ea',
+        'A200': '3a7bff',
+        'A400': '1f68ff',
+        'A700': '135ffe',
+        'contrastDefaultColor': 'light',
+        'contrastDarkColors': ['50', '100', '200', '300', '400', 'A100']
+      });
 
       $mdThemingProvider
 
-            .theme('default')
-            .primaryPalette('blue-grey');
+        .theme('default')
+        .primaryPalette('oncasePalette');
 
       $routeProvider
 
-            .when('/', {
-                templateUrl : 'app/pages/home/index.html',
-                controller  : 'homeCtrl'
-            })
+        .when('/', {
+        templateUrl: 'app/pages/home/index.html',
+        controller: 'homeCtrl'
+      })
 
-            .when('/about', {
-                templateUrl : 'app/pages/about/index.html',
-                controller  : 'aboutCtrl'
-            })
+      .when('/about', {
+        templateUrl: 'app/pages/about/index.html',
+        controller: 'aboutCtrl'
+      })
 
-            .when('/services', {
-                templateUrl : 'app/pages/services/index.html',
-                controller  : 'servicesCtrl'
-            })
+      .when('/services', {
+        templateUrl: 'app/pages/services/index.html',
+        controller: 'servicesCtrl'
+      })
 
-            .when('/products', {
-                templateUrl : 'app/pages/products/index.html',
-                controller  : 'productsCtrl'
-            })
+      .when('/products', {
+        templateUrl: 'app/pages/products/index.html',
+        controller: 'productsCtrl'
+      })
 
-            .when('/clients-and-partners', {
-                templateUrl : 'app/pages/clipar/index.html',
-                controller  : 'cliParCtrl'
-            })
+      .when('/clients-and-partners', {
+        templateUrl: 'app/pages/clipar/index.html',
+        controller: 'cliParCtrl'
+      })
 
-            .when('/contact', {
-                templateUrl : 'app/pages/contact/index.html',
-                controller  : 'contactCtrl'
-            })
+      .when('/contact', {
+        templateUrl: 'app/pages/contact/index.html',
+        controller: 'contactCtrl'
+      })
 
-            .otherwise({
-              redirectTo: '/'
-            });
+      .otherwise({
+        redirectTo: '/'
+      });
 
       $locationProvider.hashPrefix('!');
       $locationProvider.html5Mode(true);
 
-  })
+    })
 
 })();
