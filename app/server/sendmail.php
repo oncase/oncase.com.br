0a0e863d77b91cee17647cbf684eb75e73cfa76e
Marcello Pontes <balaum@gmail.com>
2015-07-24 14:27:37 +0200

    email test

--- app/server/sendmail.php
+++ app/server/sendmail.php
@@ -11,6 +11,7 @@ set_include_path(get_include_path() . PATH_SEPARATOR . $path);
 
 require_once "../../../Mail/Mail.php";
 
+$dataJS = json_decode(file_get_contents("php://input"));
 
 $host = "ssl://mail.oncase.com.br";
 $username = "no-reply@oncase.com.br";
@@ -22,18 +23,18 @@ $email_subject = "Contato atraves do site" ;
 $email_body = "Contato atraves do website";
 $email_address = "balaum@gmail.com";
 
-$name = $_POST["name"];
+$name = $dataJS->name;
 $email = $_POST["email"];
 $tel = $_POST["tel"];
 $msg = nl2br($_POST["msg"]);
 
-$email_body .= "<style type=\"text/css\">  *{ font-size:22px; font-family: 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; font-weight: 300; }</style>";
-$email_body .= "<br /><hr /><br />";
+$email_body .= "<style type=\"text/css\">  .oncase-email{ font-size:22px; font-family: 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; font-weight: 300; }</style>";
+$email_body .= "<p class='oncase-email'><br /><hr /><br />";
 $email_body .= "<br /><strong>Nome:</strong> ".name;
 $email_body .= "<br /><strong>email:</strong> ".email ;
 $email_body .= "<br /><strong>tel:</strong> ".tel;
 $email_body .= "<br /><strong>msg:</strong> <br />".msg;
-
+$email_body .= "</p>";
 
 $headers = array ('MIME-Version' => '1.0\r\n','Content-Type' => "text/html; charset=UTF-8\r\n",'From' => $email_from, 'To' => $to, 'Subject' => $email_subject, 'Reply-To' => $email_address);
 $smtp = Mail::factory('smtp', array ('host' => $host, 'port' => $port, 'auth' => true, 'username' => $username, 'password' => $password));
