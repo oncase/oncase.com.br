8ddbf70b257c3d13934fbe3c4b184297e61afd36
Marcello Pontes <balaum@gmail.com>
2015-07-24 14:52:29 +0200

    Unused to php. Added $ before some variables

--- app/server/sendmail.php
+++ app/server/sendmail.php
@@ -24,16 +24,16 @@ $email_body = "Contato atraves do website";
 $email_address = "balaum@gmail.com";
 
 $name = $dataJS->name;
-$email = $_POST["email"];
-$tel = $_POST["tel"];
-$msg = nl2br($_POST["msg"]);
+$email = $dataJS->email;
+$tel = $dataJS->tel;
+$msg = nl2br($dataJS->msg;);
 
 $email_body .= "<style type=\"text/css\">  .oncase-email{ font-size:22px; font-family: 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; font-weight: 300; }</style>";
 $email_body .= "<p class='oncase-email'><br /><hr /><br />";
-$email_body .= "<br /><strong>Nome:</strong> ".name;
-$email_body .= "<br /><strong>email:</strong> ".email ;
-$email_body .= "<br /><strong>tel:</strong> ".tel;
-$email_body .= "<br /><strong>msg:</strong> <br />".msg;
+$email_body .= "<br /><strong>Nome:</strong> ".$name;
+$email_body .= "<br /><strong>email:</strong> ".$email ;
+$email_body .= "<br /><strong>tel:</strong> ".$tel;
+$email_body .= "<br /><strong>msg:</strong> <br />".$msg;
 $email_body .= "</p>";
 
 $headers = array ('MIME-Version' => '1.0','Content-Type' => "text/html; charset=UTF-8",'From' => $email_from, 'To' => $to, 'Subject' => $email_subject, 'Reply-To' => $email_address);
