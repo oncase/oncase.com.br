b585a3e2c1da4d663056db59b8647f14dab0c737
Marcello Pontes <balaum@gmail.com>
2015-07-24 12:26:09 +0200

    sendmail.php

---
+++ app/server/sendmail.php
@@ -0,0 +1,33 @@
+<?php
+
+
+error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
+
+
+set_include_path("." . PATH_SEPARATOR . ($UserDir = dirname($_SERVER['DOCUMENT_ROOT'])) . "/pear/php" . PATH_SEPARATOR . get_include_path());
+require_once "Mail.php";
+
+
+$host = "ssl://mail.oncase.com.br";
+$username = "no-reply@oncase.com.br";
+$password = "Oncase12";
+$port = "465";
+$to = "marcello@oncase.com.br";
+$email_from = "no-reply@oncase.com.br";
+$email_subject = "Contato atraves do site" ;
+$email_body = "Oi, Marcello, tudo bem?" ;
+$email_address = "balaum@gmail.com";
+
+
+$headers = array ('From' => $email_from, 'To' => $to, 'Subject' => $email_subject, 'Reply-To' => $email_address);
+$smtp = Mail::factory('smtp', array ('host' => $host, 'port' => $port, 'auth' => true, 'username' => $username, 'password' => $password));
+$mail = $smtp->send($to, $headers, $email_body);
+
+
+if (PEAR::isError($mail)) {
+	echo("<p>" . $mail->getMessage() . "</p>");
+} else {
+	echo("<p>Message successfully sent!</p>");
+}
+
+?>
