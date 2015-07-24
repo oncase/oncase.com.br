575b93e236d599e06810783b3b3a0fd2a2e35132
Marcello Pontes <balaum@gmail.com>
2015-07-24 15:01:05 +0200

    email styling

--- app/server/sendmail.php
+++ app/server/sendmail.php
@@ -17,7 +17,7 @@ $host = "ssl://mail.oncase.com.br";
 $username = "no-reply@oncase.com.br";
 $password = "Oncase12";
 $port = "465";
-$to = "marcello@oncase.com.br";
+$to = "balaum@gmail.com";
 $email_from = "no-reply@oncase.com.br";
 $email_subject = "Contato atraves do site" ;
 $email_body = "Contato atraves do website";
@@ -26,14 +26,14 @@ $email_address = "balaum@gmail.com";
 $name = $dataJS->name;
 $email = $dataJS->email;
 $tel = $dataJS->tel;
-$msg = nl2br($dataJS->msg;);
+$msg = nl2br($dataJS->msg);
 
-$email_body .= "<style type=\"text/css\">  .oncase-email{ font-size:22px; font-family: 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; font-weight: 300; }</style>";
-$email_body .= "<p class='oncase-email'><br /><hr /><br />";
-$email_body .= "<br /><strong>Nome:</strong> ".$name;
-$email_body .= "<br /><strong>email:</strong> ".$email ;
-$email_body .= "<br /><strong>tel:</strong> ".$tel;
-$email_body .= "<br /><strong>msg:</strong> <br />".$msg;
+
+$email_body .= "<p style='font-size:22px;font-family: 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial, 'Lucida Grande', sans-serif; font-weight: 300;'><br /><hr /><br />";
+$email_body .= "<br /><strong>Nome:</strong><br />".$name;
+$email_body .= "<br /><br /><strong>email:</strong><br />".$email ;
+$email_body .= "<br /><br /><strong>tel:</strong><br />".$tel;
+$email_body .= "<br /><br /><strong>msg:</strong><br />".$msg;
 $email_body .= "</p>";
 
 $headers = array ('MIME-Version' => '1.0','Content-Type' => "text/html; charset=UTF-8",'From' => $email_from, 'To' => $to, 'Subject' => $email_subject, 'Reply-To' => $email_address);
