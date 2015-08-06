b105815dbcdda945ded9e900169188595e4fa8e1
Marcello Pontes <balaum@gmail.com>
2015-08-06 11:12:11 +0200

    Key changes and Services page
     - removed local css from about html page
     - removed some extra spacing from about, so the last card would
    overlap a little bit of the footer
     - delivered services section
     - added to servicesctrl a method to access selectedIndex from parent
    scope - needed to navigate to contact
     - Changed charts.svg main background color to green - and foreground
    color to white to keep good contrast
     - added labtransp image to be linked from the services page
     - added a pentaho-circle svg to serve as an icon on pages

--- app/pages/services/servicesCtrl.js
+++ app/pages/services/servicesCtrl.js
@@ -2,9 +2,17 @@
 
   angular.module('servicesModule',[]).
 
-  controller('servicesCtrl',['jumboBackgroundService','content',function(jumboBackgroundService,content){
-  	jumboBackgroundService.setSmall(true);
-    jumboBackgroundService.setVisible();
+  controller('servicesCtrl',['jumboBackgroundService','content','$scope',function(jumboBackgroundService,content,$scope){
+	  	jumboBackgroundService.setSmall(true);
+	    jumboBackgroundService.setVisible();
+
+	    this.goTo = function(idx){
+	    	$scope.$parent.selectedIndex = idx;
+	    	return $scope.$parent.selectedIndex;
+	    };
+
   }]);
+
+
 
 })();
