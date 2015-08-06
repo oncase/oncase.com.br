873c0fce1fa1441ba98b7b663253114746f09947
Marcello Pontes <balaum@gmail.com>
2015-08-06 13:16:46 +0200

    Minor css changes and the whole new Products section
     - separated oncase-side-img-container from the old .historico class to
    reuse it without that specific image;
     - added the whole Products page with content;
     - added technologies logo - pentaho/vertica/mongodb
     - added a class to handle mouseover saturation over a previously
    desaturated image with CSS

--- app/pages/products/productsCtrl.js
+++ app/pages/products/productsCtrl.js
@@ -2,9 +2,15 @@
 
   angular.module('productsModule',[]).
 
-  controller('productsCtrl',['jumboBackgroundService','content',function(jumboBackgroundService,content){
+  controller('productsCtrl',['jumboBackgroundService','content','$scope',function(jumboBackgroundService,content,$scope){
   	jumboBackgroundService.setSmall(true);
     jumboBackgroundService.setVisible();
+
+	    this.goTo = function(idx){
+	    	$scope.$parent.selectedIndex = idx;
+	    	return $scope.$parent.selectedIndex;
+	    };
+
   }]);
 
 })();
