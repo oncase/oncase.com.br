3b9a41e54e1961d153ca6f3b05e195cf71ff2a93
Vitor Torreao <vitor.albuquerque@oncase.com.br>
2015-10-16 12:54:31 -0300

    Internacionalização parcial da página de Quem Somos

--- app/pages/messages.js
+++ app/pages/messages.js
@@ -25,6 +25,21 @@ msgs["en"] = {
 		"CAROUSEL_INTELLIGENCE" : "Intelligence",
 		"CAROUSEL_WEARE" : "We're cloud",
 		"CAROUSEL_CLOUD_SPECIALISTS" : "Specialists"
+	},
+
+	about : {
+		"HEADING_MORE" : "More",
+		"HEADING_ABOUT_US" : "About us",
+		"HEADING_RECIPE" : "Receita para o sucesso",
+		"HEADING_RECIPE_UPPERTEXT" : "Somamos com sua equipe para dividirmos os problemas, subtraindo os riscos e multiplicando resultados!",
+		"HEADING_RECIPE_LOWERTEXT" : "Ajudamos você a entender melhor os dados de sua empresa, transformando-os em informação útil para a tomada de decisão, reduzindo perdas, minimizando custos, aumentando receitas e otimizando o seu bem mais precioso: O seu tempo. Esse é o nosso negócio.",
+		"HEADING_TARGET" : "No alvo",
+		"HEADING_TARGET_UPPERTEXT" : "Esse é o nosso maior objetivo, fazer com que nossos clientes possam gerir seus negócios com a informação na ponta do lápis, ou seja, em seus dispositivos móveis, a qualquer hora (alta disponibilidade da informação), em qualquer lugar (mobilidade).",
+		"HEADING_TARGET_LOWERTEXT" : "Não perca mais tempo, entre em contato conosco e vamos revolucionar a sua informação, o seu negócios, impressionar seus clientes.",
+		"HEADING_CONTACT_BUTTON" : "Entre em contato",
+		"WHAT_WE_DO_HEADING" : "What do we do?",
+		"WHAT_WE_DO_FIRST_PG" : "Somos uma consultoria especializada em soluções de Business Analytics, Business Intelligence e Big Data, com ênfase nas edições Community e Enterprise da <strong>Plataforma Pentaho</strong>.",
+		"WHAT_WE_DO_SECOND_PG" : "Atendemos todo o ciclo da informação, desde a extração, transformação e carga de dados estruturados e não estruturados, integrando com os principais Bando de Dados do mercado, proporcionado uma análise mais detalhada da informação, visando a criação de relatórios com informações consistentes e a publicação e distribuição dentro da corporação em painéis múltiplos, no formato de dashboards, utilizando metologias de desenvolimento ágeis e tecnologia de ponta."
 	}
 
 };
@@ -54,6 +69,21 @@ msgs["pt"] = {
 		"CAROUSEL_INTELLIGENCE" : "Intelig\u00EAncia",
 		"CAROUSEL_WEARE" : "Somos especialistas",
 		"CAROUSEL_CLOUD_SPECIALISTS" : "em Cloud"
+	},
+
+	about : {
+		"HEADING_MORE" : "Mais",
+		"HEADING_ABOUT_US" : "Sobre nós",
+		"HEADING_RECIPE" : "Receita para o sucesso",
+		"HEADING_RECIPE_UPPERTEXT" : "Somamos com sua equipe para dividirmos os problemas, subtraindo os riscos e multiplicando resultados!",
+		"HEADING_RECIPE_LOWERTEXT" : "Ajudamos você a entender melhor os dados de sua empresa, transformando-os em informação útil para a tomada de decisão, reduzindo perdas, minimizando custos, aumentando receitas e otimizando o seu bem mais precioso: O seu tempo. Esse é o nosso negócio.",
+		"HEADING_TARGET" : "No alvo",
+		"HEADING_TARGET_UPPERTEXT" : "Esse é o nosso maior objetivo, fazer com que nossos clientes possam gerir seus negócios com a informação na ponta do lápis, ou seja, em seus dispositivos móveis, a qualquer hora (alta disponibilidade da informação), em qualquer lugar (mobilidade).",
+		"HEADING_TARGET_LOWERTEXT" : "Não perca mais tempo, entre em contato conosco e vamos revolucionar a sua informação, o seu negócios, impressionar seus clientes.",
+		"HEADING_CONTACT_BUTTON" : "Entre em contato",
+		"WHAT_WE_DO_HEADING" : "O que fazemos?",
+		"WHAT_WE_DO_FIRST_PG" : "Somos uma consultoria especializada em soluções de Business Analytics, Business Intelligence e Big Data, com ênfase nas edições Community e Enterprise da <strong>Plataforma Pentaho</strong>.",
+		"WHAT_WE_DO_SECOND_PG" : "Atendemos todo o ciclo da informação, desde a extração, transformação e carga de dados estruturados e não estruturados, integrando com os principais Bando de Dados do mercado, proporcionado uma análise mais detalhada da informação, visando a criação de relatórios com informações consistentes e a publicação e distribuição dentro da corporação em painéis múltiplos, no formato de dashboards, utilizando metologias de desenvolimento ágeis e tecnologia de ponta."
 	}
 
 };
